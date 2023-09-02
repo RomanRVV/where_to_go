ccf8c1ea2c119ffd59491638f0fe5a22990f0bf7
RomanRVV <rvachyov.roma@yandex.ru>
2023-09-02 17:25:08 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -5,3 +5,8 @@ from .models import *
 @ admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['title']
+
+
+@ admin.register(PlacesImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ['title']
--- places/models.py
+++ places/models.py
@@ -14,3 +14,9 @@ class Place(models.Model):
         return self.title
 
 
+class PlacesImage(models.Model):
+    title = models.CharField(max_length=200, blank=True, verbose_name='Заголовок')
+    image = models.ImageField(upload_to='images', verbose_name="Изображение места")
+
+    def __str__(self):
+        return self.title
