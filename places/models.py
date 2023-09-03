1e6bd30c7fe2e91f50a9864a469c669bb2001527
RomanRVV <rvachyov.roma@yandex.ru>
2023-09-03 14:58:22 +0300

    add WYSIWYG

--- places/models.py
+++ places/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 # Create your models here.
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200, blank=True, verbose_name='Заголовок')
     description_short = models.TextField(blank=True, verbose_name='Короткое описание')
-    description_long = models.TextField(blank=True, verbose_name='Длинное описание')
+    description_long = HTMLField(blank=True, verbose_name='Длинное описание')
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from where_to_go import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,8 +23,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_main),
-    path('places/<int:place_id>/', views.show_place, name='places')
+    path('places/<int:place_id>/', views.show_place, name='places'),
+    path('tinymce/', include('tinymce.urls'))
 ]
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
