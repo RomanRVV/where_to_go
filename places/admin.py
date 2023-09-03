e6aac6ff4c24cb90434dbaac827a86f8488860db
RomanRVV <rvachyov.roma@yandex.ru>
2023-09-03 15:28:16 +0300

    add readme and requirements

--- places/admin.py
+++ places/admin.py
@@ -25,6 +25,7 @@ class ImageInline(SortableTabularInline):
 @ admin.register(Place)
 class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     list_display = ['title']
+    search_fields = ['title']
     inlines = [
         ImageInline,
     ]
