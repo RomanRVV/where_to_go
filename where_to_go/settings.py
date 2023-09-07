01f262e4716bd40d3ebba15413a712e56d0407d2
RomanRVV <rvachyov.roma@yandex.ru>
2023-09-07 20:18:06 +0300

    Add STATIC_ROOT

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = Env()
 env.read_env()
 
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
@@ -122,13 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / 'static'
-
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+    os.path.join(BASE_DIR, 'static'),
+    ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
